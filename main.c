0a0b9d93b24903ade57bf089c4e022f964bc5924
D'Mita Levy <D'Mita_Levy@ncci.com>
2019-01-11 17:57:32 -0500

    cleaned up some comments

--- main.c
+++ main.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-
-
-// A recursive binary search function. It returns 
-// location of x in given array arr[l..r] is present, 
-// otherwise -1 
 int binarySearch(int arr[], int l, int r, int x) 
 { 
     if (r >= l) { 
